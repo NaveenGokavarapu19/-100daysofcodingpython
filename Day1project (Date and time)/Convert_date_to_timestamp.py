0f31b4e709b22342324f108549a4f88cd7f4aafe
Naveen <gokavarapu.naveen97@gmail.com>
2020-06-10 11:11:39 +0530

    Made a few changes regarding the structure of the program

--- Day1project (Date and time)/Convert_date_to_timestamp.py
+++ Day1project (Date and time)/Convert_date_to_timestamp.py
@@ -1,5 +1,17 @@
 import time
 import datetime
 
-string = "20/01/2020"
-print(time.mktime(datetime.datetime.strptime(string,"%d/%m/%Y").timetuple()))
+
+# Define function to take input as string
+def take_input():
+    time_required= input("Please enter the date in the dd/mm/yyyy :")
+    return time_required
+
+# Define a function to process the date to timestamp
+def return_date(date):
+    return time.mktime(datetime.datetime.strptime(date, "%d/%m/%Y").timetuple())
+
+
+time_entered = take_input()
+time_stamp = return_date(time_entered)
+print(" the time stamp is {} ".format(time_stamp))
