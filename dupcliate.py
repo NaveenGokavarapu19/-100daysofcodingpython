54208418a7097c033f83f06e52b766c6f8c20255
NaveenGokavarapu19 <66323587+NaveenGokavarapu19@users.noreply.github.com>
2021-05-19 16:09:59 +0530

    Add files via upload

--- dupcliate.py
+++ dupcliate.py
@@ -4,65 +4,97 @@ import datetime as dt
 import shutil
 import time
 
-
-def file_compress():
-        while True:
-            source_dir = "/home/naveen/testing"
-            os.chdir("/home/naveen/testing")
-
-            file_list = sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
-            file_list.remove("connect-log.log")
-
-            files_to_be_appended = [file for file in file_list if "tar" not in file]
-            print(len(file_list))
-            files_to_moved = [ file for file in file_list if "tar"  in file]
-            to_move_files = []
-            try:
-                time_stamp = files_to_be_appended[0].split(".")[2]
-                for i in range(5):
-                    to_move_files.append(files_to_be_appended[i])
-                process = subprocess.run(["tar", "-czf", f"connnect-log.{time_stamp}.tar.gz", files_to_be_appended[0],
-                                          files_to_be_appended[1],
-                                          files_to_be_appended[2],
-                                          files_to_be_appended[3], files_to_be_appended[4]], capture_output=True)
-                for file_name in to_move_files:
-                    os.remove(f"{file_name}")
-
-                for file_name in files_to_moved:
-                    shutil.move(os.path.join(source_dir, file_name), "/tmp")
-            except IndexError:
-                # left_file_list = os.listdir('.')
-                # left_file_list.remove("connect-log.log")
-                # shutil.move(os.path.join(source_dir, left_file_list[1]), "/tmp")
-                time.sleep(5)
-
-            else:
-
-                print(len(to_move_files))
-
-                print(files_to_be_appended)
-                print(len(files_to_be_appended))
-                time.sleep(2)
-
-            finally:
-                time.sleep(2)
+LOGS_PATH = "/home/naveen/testing"
+DESTINATION = "/tmp/logs"
 
 
+def change_to_current_path(to_change_path):
+    """
+    Function to change to required Directory
+    :param to_change_path: PLease change LOGS_PATH variable to change the path
+    :return: no return
+    """
+    os.chdir(to_change_path)
 
 
+def get_file_list_without_current_log():
+    """
+    Lists the files created with earliest files created first as list
+    Removing current log file from the list
+    :return:
+    """
+    full_list = sorted(filter(os.path.isfile, os.listdir('.')), key=os.path.getmtime)
+    full_list.remove("connect-log.log")
+    return full_list
 
 
+def filter_list(to_process_list):
+    """
+    Filter files according to the extension and appending them to there respective lists
+    :param to_process_list:
+    :return: tuple of  lists which contains names of log files and tar files
+    """
+    log_file_list = [file for file in to_process_list if "tar" not in file]
+    tar_file_list = [file for file in to_process_list if "tar" in file]
+    return log_file_list, tar_file_list
 
 
+def compress_files(time_stamp, files_to_be_appended):
+    """
+    Using subprocess module to run tar command to tar first 5 files in the list
+    Note: Tar file has limitation regarding naming. Only time-stamp with four divisions are accepted
+    Note1: To debug print the process variable
+    :param time_stamp: can be generated using datetime module with method datetime.datetime.now()
+    :param files_to_be_appended:
+    :return: no return
+    """
+    process = subprocess.run(["tar", "-czf", f"connect-log.{time_stamp}.tar.gz", files_to_be_appended[0],
+                              files_to_be_appended[1],
+                              files_to_be_appended[2],
+                              files_to_be_appended[3], files_to_be_appended[4]], capture_output=True)
 
 
+def remove_current_logs_and_mv_comp_files(to_move_files, files_to_be_moved):
+    """
+    moves tar files to specified location and removes source files
+    :param to_move_files:
+    :param files_to_be_moved:
+    :return: no return
+    """
+    [os.remove(f"{file_name}") for file_name in to_move_files]
+    [shutil.move(os.path.join(LOGS_PATH, file_name), DESTINATION) for file_name in files_to_be_moved]
 
 
+def compress_logs():
+    while True:
+        change_to_current_path(LOGS_PATH)
+        file_list = get_file_list_without_current_log()
+        files_to_be_appended, files_to_be_moved = filter_list(file_list)
+        print(files_to_be_moved)
+
+        try:
+            time_stamp = files_to_be_appended[0].split(".")[2]
+            to_move_files = [files_to_be_appended[i] for i in range(5)]
+            compress_files(time_stamp, files_to_be_appended)
+            remove_current_logs_and_mv_comp_files(to_move_files, files_to_be_moved)
 
 
+        except Exception:
+
+            time.sleep(5)
+            print("Hello")
+
+        try:
+            [shutil.move(os.path.join(LOGS_PATH, file_name), DESTINATION) for file_name in files_to_be_moved]
+
+        except:
+            time.sleep(1)
+
+        else:
+            time.sleep(2)
+
+        finally:
+            time.sleep(1)
 
 
-file_compress()
-
-
-
+compress_logs()
